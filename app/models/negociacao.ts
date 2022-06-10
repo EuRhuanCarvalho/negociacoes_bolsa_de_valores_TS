b1bec20a3fdec68df5b8a562aeabd86818499821
EuRhuanCarvalho <rhuan_eugenio@hotmail.com>
2022-06-09 23:35:20 -0300

    criaçao negociacao

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,31 +1,31 @@
 export class Negociacao {
 
-    #data;
-    #quantidade;
-    #valor;
-    #volume;
+    private _data: Date;
+    private _quantidade: number;
+    private _valor: number;
+    
 
-    constructor(data, quantidade, valor) {
-        this.#data = data;
-        this.#quantidade = quantidade;
-        this.#valor = valor;
-
-    }
-
-    get data() { 
-        return this.#data;
-    }
-
-    get quantidade() {
-        return this.#quantidade;
-    }
-
-    get valor() {
-        return this.#valor;
+    constructor(data: Date, quantidade: number, valor: number) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
 
     }
 
-    get volume() {
-        return this.#quantidade * this.#valor;
+    get data(): Date { 
+        return this._data;
+    }
+
+    get quantidade(): number {
+        return this._quantidade;
+    }
+
+    get valor(): number {
+        return this._valor;
+
+    }
+
+    get volume(): number {
+        return this._quantidade * this._valor;
     }
 }
