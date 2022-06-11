c7fffbe8368ea9b3b548a8ea63c30b4e93116bb9
EuRhuanCarvalho <rhuan_eugenio@hotmail.com>
2022-06-10 22:17:09 -0300

    refatorando negociacao

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,26 +1,13 @@
 export class Negociacao {
  
 
-    constructor(
-        private _data: Date, 
-        private _quantidade: number, 
-        private _valor: number       
-        ) {}
-
-    get data(): Date { 
-        return this._data;
-    }
-
-    get quantidade(): number {
-        return this._quantidade;
-    }
-
-    get valor(): number {
-        return this._valor;
-
-    }
+    constructor( // muito cuidado pois o readonly não impossibilita completamente a alteração. Usado metodos Set possibilitam isso. Ex: SetDate
+        public readonly data: Date, 
+        public readonly quantidade: number, 
+        public readonly valor: number       
+    ) {}
 
     get volume(): number {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 }
